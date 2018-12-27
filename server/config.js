6ccef20672eb9d4b768f83d5c6b2fe173b4a85ea
endriu <aolaszewski@gmail.com>
2018-12-27 21:33:51 +0100

    add server config

--- server/config.js
+++ server/config.js
@@ -1,5 +1,5 @@
 const config = {
-  mongoURL: process.env.MONGO_URL || 'mongodb://localhost:27017/mern-starter',
+  mongoURL: process.env.MONGO_URL || 'mongodb://andrzej:admin1234@ds145194.mlab.com:45194/kanban',
   port: process.env.PORT || 8000,
 };
 
